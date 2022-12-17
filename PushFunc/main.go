d251f05ebafa988da57a32a9728624a0717206b0
afzal442 <afzal442@gmail.com>
2022-12-17 23:43:08 +0530

    adds struct functionality
    Signed-off-by: afzal442 <afzal442@gmail.com>

--- PushFunc/main.go
+++ PushFunc/main.go
@@ -10,12 +10,14 @@ type statStack interface {
 	pushfunc(string)
 }
 
+type ropt struct{}
+
 type stat struct {
 	curr string `json`
 	stat int32  `json`
 }
 
-func pushfunc(ctx context.Context, temp string) string {
+func (r ropt) pushfunc(ctx context.Context, temp string) string {
 	ctx, canc := context.WithTimeout(ctx, time.Millisecond*500)
 	defer canc()
 	return temp
@@ -24,8 +26,9 @@ func pushfunc(ctx context.Context, temp string) string {
 func main() {
 	t := "uot"
 	ctx := context.Background()
+	r := &ropt{}
 
-	f := pushfunc(ctx, t)
+	f := r.pushfunc(ctx, t)
 
 	fmt.Println(f)
 }
