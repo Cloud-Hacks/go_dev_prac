8b78203afa8f1449ebe934d2a7ef1b18bca06c7a
afzal442 <afzal442@gmail.com>
2022-12-18 23:00:46 +0530

    adds constructor
    Signed-off-by: afzal442 <afzal442@gmail.com>

--- PushFunc/main.go
+++ PushFunc/main.go
@@ -17,18 +17,34 @@ type stat struct {
 	stat int32  `json`
 }
 
+func addStat() stat {
+	return stat{
+		curr: "yi",
+		stat: 65,
+	}
+}
+
 func (r ropt) pushfunc(ctx context.Context, temp string) string {
 	ctx, canc := context.WithTimeout(ctx, time.Millisecond*500)
 	defer canc()
 	return temp
 }
 
+func (t stat) rpquest(tmp string) (string, int32) {
+	t.curr = tmp
+	return t.curr, t.stat
+}
+
 func main() {
 	t := "uot"
 	ctx := context.Background()
-	r := &ropt{}
 
+	s := addStat()
+	x, y := s.rpquest(t)
+
+	r := &ropt{}
 	f := r.pushfunc(ctx, t)
 
 	fmt.Println(f)
+	fmt.Println(x, y)
 }
