81604febe43e3d127471b24e800f20cd6ac8658c
afzal442 <afzal442@gmail.com>
2022-12-19 23:46:58 +0530

    adds more usage
    Signed-off-by: afzal442 <afzal442@gmail.com>

--- Contxt_func/main.go
+++ Contxt_func/main.go
@@ -7,20 +7,40 @@ import (
 	"time"
 )
 
+type Res struct {
+	v  int
+	er error
+}
+
+// context allows you to maintain the consistency among the fetch requests to third party
 func fetchThirdPartyDataAnything() (int, error) {
-	time.Sleep(50 * time.Second)
+	time.Sleep(5 * time.Millisecond)
 
 	return 777, nil
 }
 
 func fetchDataAny(ctx context.Context, Uid int) (int, error) {
-	val, err := fetchThirdPartyDataAnything()
+	ctx, canc := context.WithTimeout(ctx, time.Millisecond*200)
+	defer canc()
 
-	if err != nil {
-		return 0, err
+	resp := make(chan Res)
+
+	go func() {
+		val, err := fetchThirdPartyDataAnything()
+		resp <- Res{
+			v:  val,
+			er: err,
+		}
+	}()
+
+	for {
+		select {
+		case <-ctx.Done():
+			return 0, fmt.Errorf("error fetching from 3rd party")
+		case res := <-resp:
+			return res.v, res.er
+		}
 	}
-
-	return val, nil
 
 }
 
@@ -33,6 +53,6 @@ func main() {
 		log.Fatal(err)
 	}
 
-	fmt.Printf("res %d", val)
+	fmt.Printf("res %d \n", val)
 	fmt.Println(time.Since(strt))
 }
