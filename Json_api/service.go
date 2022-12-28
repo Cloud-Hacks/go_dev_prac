e0544c6c0d7048651fbe0e03f181e0c8b21590dd
afzal442 <afzal442@gmail.com>
2022-12-28 22:51:56 +0530

    adds json decoder
    Signed-off-by: afzal442 <afzal442@gmail.com>

--- Json_api/service.go
+++ Json_api/service.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"context"
+	"encoding/json"
 	"net/http"
 )
 
@@ -9,12 +10,31 @@ type Service interface {
 	GetFactSvc(context.Context) (*myFact, error)
 }
 
-func GetFactSvc(context.Context) (*myFact, error) {
+type myFact struct{}
+
+type factSvc struct {
+	url string
+}
+
+func GetUrl(url string) Service {
+	return &factSvc{
+		url: url,
+	}
+}
+
+func (s *factSvc) GetFactSvc(context.Context) (*myFact, error) {
 	resp, err := http.Get("http://catfact.ninja/fact")
+	resp.Body.Close()
 
 	if err != nil {
 		return nil, err
 	}
 
-	return resp, nil
+	f := &myFact{}
+
+	if err := json.NewDecoder(resp.Body).Decode(f); err != nil {
+		return nil, err
+	}
+
+	return f, nil
 }
