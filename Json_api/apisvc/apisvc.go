9884ce75795da7b5e8a7abd0f4e2623cca58dfdd
afzal442 <afzal442@gmail.com>
2023-01-06 23:33:07 +0530

    completes total project
    Signed-off-by: afzal442 <afzal442@gmail.com>

--- Json_api/apisvc/apisvc.go
+++ Json_api/apisvc/apisvc.go
@@ -4,28 +4,38 @@ import (
 	"context"
 	"encoding/json"
 	"net/http"
+
+	"github.com/Cloud-Hacks/go_dev_prac/json_api/service"
 )
 
 type Apisvr struct {
-	svc Service.service
+	svc service.Service
 }
 
-func NewApiReq(svc Service.service) Apisvr {
+func NewApiReq(svc service.Service) Apisvr {
 	return Apisvr{
 		svc: svc,
 	}
 }
 
+func (s *Apisvr) Handler(listener string) error {
+	http.HandleFunc("/", s.handleGetFact)
+	return http.ListenAndServe(listener, nil)
+}
+
 func (s *Apisvr) handleGetFact(w http.ResponseWriter, r *http.Request) {
-	fact, err = s.svc.GetFactSvc(context.Background())
+	fact, err := s.svc.GetFactSvc(context.Background())
 
 	if err != nil {
-
+		writeJson(w, http.StatusExpectationFailed, map[string]any{"error": err.Error()})
+		return
 	}
+
+	writeJson(w, http.StatusAccepted, fact)
 }
 
 func writeJson(w http.ResponseWriter, i int, t any) error {
-	w.WriteHeader(s)
+	w.WriteHeader(i)
 	w.Header().Add("Content-type", "application/json")
 
 	return json.NewEncoder(w).Encode(t)
--- Json_api/main.go
+++ Json_api/main.go
@@ -1,18 +1,22 @@
 package main
 
 import (
-	"context"
-	"fmt"
+	"log"
 
+	"github.com/Cloud-Hacks/go_dev_prac/json_api/apisvc"
 	"github.com/Cloud-Hacks/go_dev_prac/json_api/service"
 )
 
 func main() {
-	ctx := context.Background()
+	// ctx := context.Background()
 	s := service.GetUrl("http://catfact.ninja/fact")
 
-	f, _ := s.GetFactSvc(ctx)
+	// f, _ := s.GetFactSvc(ctx)
 
-	fmt.Println(*f)
+	apisvr := apisvc.NewApiReq(s)
+
+	log.Fatal(apisvr.Handler(":5000"))
+
+	// fmt.Println(*f)
 
 }
