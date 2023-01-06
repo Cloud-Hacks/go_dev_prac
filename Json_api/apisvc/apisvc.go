a80f82c4c1a10a46ef7113767afd41518280f603
afzal442 <afzal442@gmail.com>
2023-01-06 23:08:35 +0530

    adds json response writer
    Signed-off-by: afzal442 <afzal442@gmail.com>

--- Json_api/apisvc/apisvc.go
+++ Json_api/apisvc/apisvc.go
@@ -1,5 +1,33 @@
 package apisvc
 
+import (
+	"context"
+	"encoding/json"
+	"net/http"
+)
+
 type Apisvr struct {
 	svc Service.service
+}
+
+func NewApiReq(svc Service.service) Apisvr {
+	return Apisvr{
+		svc: svc,
+	}
+}
+
+func (s *Apisvr) handleGetFact(w http.ResponseWriter, r *http.Request) {
+	fact, err = s.svc.GetFactSvc(context.Background())
+
+	if err != nil {
+
+	}
+}
+
+func writeJson(w http.ResponseWriter, i int, t any) error {
+	w.WriteHeader(s)
+	w.Header().Add("Content-type", "application/json")
+
+	return json.NewEncoder(w).Encode(t)
+	// return json.Marshal()
 }
