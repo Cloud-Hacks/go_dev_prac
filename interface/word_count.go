2104ca9e096fa1ae0729d779da3ac502121ab5d4
afzalbin64 <afzal442@gmail.com>
2023-03-02 00:02:10 +0530

    uodates word_count method
    Signed-off-by: afzalbin64 <afzal442@gmail.com>

---
+++ interface/word_count.go
@@ -0,0 +1,53 @@
+package main
+
+import (
+	"compress/gzip"
+	"fmt"
+	"io"
+	"os"
+	"strings"
+)
+
+func LineCount(fileName string) (int, error) {
+	file, err := os.Open(fileName)
+	if err != nil {
+		return 0, err
+	}
+	defer file.Close()
+
+	// r implements *io.file
+	var r io.Reader = file
+	// io.ReadAll(r)
+
+	var lc LineCounter
+
+	if strings.HasSuffix(fileName, ".gz") {
+		r, err = gzip.NewReader(file)
+		if err != nil {
+			return 0, err
+		}
+	}
+
+	if _, err := io.Copy(&lc, r); err != nil {
+		return 0, err
+	}
+
+	return int(lc), nil
+}
+
+type LineCounter int
+
+func (lc *LineCounter) Write(data []byte) (int, error) {
+	for _, c := range data {
+		if c == '\n' {
+			*lc++
+		}
+	}
+
+	return len(data), nil
+}
+
+func main() {
+	fmt.Println(LineCount("testdata/road.txt"))
+	fmt.Println(LineCount("testdata/aow.txt.gz"))
+}
